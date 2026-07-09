66798c13b978d8a335eac61f70f11e1eaa6007ba
Abaraajith Thirumoorthy <abaraajith.t@gmail.com>
2026-07-09 07:41:55 +0530

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about_me.html'),
+        featured: resolve(__dirname, 'featured_article.html'),
+      },
+    },
+  },
+});
